903169731b765706efd94b866fe3417cacc653ad
SoftwareEngineering314 <iain.ross2027@gmail.com>
2025-11-08 12:50:51 -0700

    you can now save and load novels

---
+++ src/main/java/com/novelreviewer/service/LibraryService.java
@@ -0,0 +1,25 @@
+package com.novelreviewer.service;
+
+import com.novelreviewer.model.Library;
+import com.novelreviewer.model.Novel;
+import com.novelreviewer.utility.FileStorage;
+
+import java.io.IOException;
+
+public class LibraryService {
+    private final Library library = new Library();
+    private final FileStorage storage = new FileStorage();
+
+    public void addNovel(String author, String title, String rating, String chapters) {
+        Novel novel = new Novel(author, rating, chapters);
+        library.addNovel(title, novel);
+    }
+    public Library getLibrary(){
+        return library;
+    }
+    public void loadLibrary() throws IOException {
+        Library loadedLibrary = storage.loadLibrary();
+        library.clear();
+        library.getNovels().putAll(loadedLibrary.getNovels());
+    }
+}
--- src/main/java/com/novelreviewer/ui/MainController.java
+++ src/main/java/com/novelreviewer/ui/MainController.java
@@ -1,20 +1,19 @@
 package com.novelreviewer.ui;
-
-import com.novelreviewer.model.Library;
-import com.novelreviewer.model.Novel;
+import com.novelreviewer.service.LibraryService;
+import com.novelreviewer.utility.FileStorage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.InputMethodEvent;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 
 public class MainController {
-    private String author;
-    private String title;
-    private String chapters;
-    private String rating;
-    private final Library library = new Library();
+    private final LibraryService libraryService = new LibraryService();
+    private final FileStorage storage = new FileStorage();
     @FXML
     private Label label;
     @FXML
@@ -27,20 +26,33 @@ public class MainController {
     private TextField ratingInput;
     @FXML
     private Button novelAddButton;
-
+    @FXML
+    private Button saveButton;
+    @FXML
+    private Button loadButton;
     @FXML
     private void handleAddNovel(ActionEvent event) {
-        String author = authorInput.getText();
-        String title = titleInput.getText();
-        String rating = ratingInput.getText();
-        String chapters = chapterInput.getText();
-        Novel novel = new Novel(author, rating, chapters);
-        Library library = new Library();
-        library.addNovel(title, novel);
+        libraryService.addNovel(authorInput.getText(), titleInput.getText(), chapterInput.getText(), ratingInput.getText());
         authorInput.clear();
         titleInput.clear();
         ratingInput.clear();
         chapterInput.clear();
+    }
+    @FXML
+    private void saveLibrary(ActionEvent event) {
+        try {
+            storage.saveLibrary(libraryService.getLibrary(), "userdata/novels/novels.json");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void loadLibrary(ActionEvent event) {
+        try{
+            libraryService.loadLibrary();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     @FXML
